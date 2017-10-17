411d6aea195877c0766bd1b79386ae692702547e
qliphortm7 <qliphortm7@gmail.com>
2017-10-17 16:05:59 +0800

    demo

---
+++ blog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from blog import models
+
+# Create your views here.
+
+
+def index(request):
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
+
+
+def article_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/article_page.html', {'article': article})
+
+
+def article_edit(request, article_id):
+    if str(article_id) == '0':
+        return render(request, 'blog/article_edit.html')
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/article_edit.html', {'article': article})
+
+
+def edit_action(request):
+    title = request.POST.get('title', 'TITLE')
+    content = request.POST.get('content', 'CONTENT')
+    article_id = request.POST.get('article_id', '0')
+    if article_id == '0':
+        models.Article.objects.create(title=title, content=content)
+        return HttpResponseRedirect('/blog/index')
+    else:
+        article = models.Article.objects.get(pk=article_id)
+        article.title = title
+        article.content = content
+        article.save()
+        return HttpResponseRedirect('/blog/index')
