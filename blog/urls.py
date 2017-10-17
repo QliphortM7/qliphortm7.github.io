084777c32f2576d6e203e9f3976d2a53b9af49fa
qliphortm7 <qliphortm7@gmail.com>
2017-10-17 16:25:41 +0800

    change a lot
    submit some html files.

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from blog import views
-
-urlpatterns = [
-    url(r'^index/$', views.index, name='index'),
-    url(r'^article/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
-    url(r'^edit/(?P<article_id>[0-9]+)$', views.article_edit, name='article_edit'),
-    url(r'^edit/action/$', views.edit_action, name='edit_action'),
-]
